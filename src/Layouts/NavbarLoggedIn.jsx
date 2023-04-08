d2d3963d1bbe060d65c21f378f8a8f5fbef0cdff
unknown <clrkdiazlee@gmail.com>
2023-04-09 01:58:09 +0800

    Added edit profiles (incomplete)

--- src/Layouts/NavbarLoggedIn.jsx
+++ src/Layouts/NavbarLoggedIn.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Navbar() {
+function NavbarLoggedIn() {
+  const navigate = useNavigate();
+  const Logout = () => {
+  axios
+      .post(`/logout`, {})
+      .then((res) => {
+        axios.defaults.headers.common['Authorization'] = ""
+        localStorage.removeItem('default-token')
+        console.log(res.data)
+        navigate("/Login")
+      })
+      .catch((error) => {
+        axios.defaults.headers.common['Authorization'] = ""
+        localStorage.removeItem('default-token')
+        console.log(error);
+      })
+    }
+
   return (
-    <div className="flex justify-between px-24 w-full shadow-md shadow-slate-200">
+    <div className="flex justify-between px-24 w-full shadow-md shadow-slate-200 z-10">
       <div className="flex items-center">
         <div className="pr-16 py-8">
           <Link to="/">
@@ -15,7 +32,7 @@ function Navbar() {
             <li>
               <Link to="/About">
                 About
-              </Link>
+              </Link> 
             </li>
             <li>
               <a href="#">Categories</a>
@@ -50,9 +67,16 @@ function Navbar() {
             />
           </form>
         </div>
+        <div className="flex">
+          <div className="px-5">
+            <button onClick={Logout} className="bg-buttonBlue border border-buttonBlue font-bold py-2 px-4 rounded-full text-white font-poppins text-sm tracking-wider">
+                Log-out
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Navbar;
+export default NavbarLoggedIn;
