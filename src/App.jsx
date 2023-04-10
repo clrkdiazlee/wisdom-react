9b193ca59253fa5b27b86a1089bad67a2af09c14
unknown <clrkdiazlee@gmail.com>
2023-04-10 21:40:07 +0800

    Added daily logs (incomplete)

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Protected from "./components/Protected";
 import StudentUploadRequirements from "./components/StudentUploadRequirements";
 import StudentProfile from "./components/StudentProfile";
 import Calendar from "./components/Calendar";
+import CalendarDailyLog from "./components/CalendarDailyLog";
 import SchoolAddStudent from "./components/SchoolAddStudent";
 import SchoolEditProfile from "./components/SchoolEditProfile";
 import StudentUploadRequirementsMOA from "./components/StudentUploadRequirementsMOA";
@@ -121,6 +122,14 @@ const App = () => {
           element={
             <Protected isSignedIn={localStorage.getItem("default-token")}>
               <Calendar />
+            </Protected>
+          }
+        />
+        <Route
+          path="/DailyLog"
+          element={
+            <Protected isSignedIn={localStorage.getItem("default-token")}>
+              <CalendarDailyLog />
             </Protected>
           }
         />
